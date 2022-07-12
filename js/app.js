6fc716eed1efe218068a4d38ff1eec721f70ecc8
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-07-12 15:59:24 +0500

    custom settings adn ad expend function

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// localStorage - bu brauzerni local xotirasi va unga text ko'rinishida istalgan narsani 
+// saqlasa bo'ladi
+// localStorage : 
+//     setItem - yangi narsa yozish uchun 
+//     getItem - narsa olish uchun 
+//     removeItem - o'chirish uchun 
+//     clear - xotirani tozalash uchun
+
+// console.log(typeof localStorage)
+// localStorage.setItem("expends", JSON.stringify([]))
+function addExpendToStorage() {
+    let cost = document.querySelector("#expendValue").value
+    let desc = document.querySelector("#expendDesc").value
+    // console.log(cost, desc)
+    let obj = JSON.parse(localStorage.getItem("expends"))
+    obj.push({
+        cost: cost,
+        desc: desc
+    })
+    // JSON.stringify(obj) bu js ovyektni json yani oddiy ko'rinishiga olib o'tish
+    localStorage.setItem("expends", JSON.stringify(obj))
+    // console.log(localStorage.getItem("expends"))
+}
+// localStorage.clear() //- butunlay tozalash
+
+
+function mainApp() {
+    let expends = JSON.parse(localStorage.getItem("expends"))
+    let list = document.querySelector("#expendList")
+    let totalExpend = 0
+    for (let item of expends) {
+        totalExpend += parseInt(item.cost)
+        let li = document.createElement("li")
+        li.className = "list-group-item d-flex justify-content-between align-items-start"
+        li.innerHTML = `
+        <div class="ms-2 me-auto">
+            <div class="fw-bold">
+                ${item.desc}
+            </div>
+        
+        </div>
+        <span class="badge bg-primary badge-lg rounded-pill fs-5">$ ${item.cost}</span>
+        `
+        list.appendChild(li)
+    }
+    document.querySelector("#totalExpend").innerText = totalExpend
+
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    mainApp()
+})
